e17d4fd5648b210be023bca7d9911b8e6875d7e1
Anesu Rirwa <anerirwa@gmail.com>
2024-01-09 00:52:04 +0200

    Scrap Multiple Pages

--- scraper.py
+++ scraper.py
@@ -19,8 +19,24 @@ class QuoteSpider(scrapy.Spider):
         QUOTE_SELECTOR = '.quote'
         TEXT_SELECTOR ='.text::text'
         AUTHOR_SELECTOR = '.author::text'
+        ABOUT_SELECTOR = '.author + a::attr("href")'
+        TAGS_SELECTOR = '.tags > .tag::text'
+        NEXT_SELECTOR = '.next a::attr("href")'
 
         for quote in response.css(QUOTE_SELECTOR):
-            pass
+            yield {
+                'text': quote.css(TEXT_SELECTOR).extract_first(),
+                'author': quote.css(AUTHOR_SELECTOR).extract_first(),
+                'about': 'https://quotes.toscrape.com' + quote.css(ABOUT_SELECTOR).extract_first(),
+                'tags': quote.css(TAGS_SELECTOR).extract(),
+            
+            }
+
+        next_page = response.css(NEXT_SELECTOR).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+            )
+
 
 
